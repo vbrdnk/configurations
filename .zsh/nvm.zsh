39d51bb3976201ce1895c3264e3aa30ebecaf5d4
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-08-22 21:13:55 -0700

    Add pnpm alias to nvm.zsh

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -1,7 +1,8 @@
 # NVM lazy load
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
   [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
-  alias nvm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && nvm'
-  alias node='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && node'
-  alias npm='unalias nvm node npm && . "$NVM_DIR"/nvm.sh && npm'
+  alias nvm='unalias nvm node npm pnpm && . "$NVM_DIR"/nvm.sh && nvm'
+  alias node='unalias nvm node npm pnpm && . "$NVM_DIR"/nvm.sh && node'
+  alias npm='unalias nvm node npm pnpm && . "$NVM_DIR"/nvm.sh && npm'
+  alias pnpm='unalias nvm node npm pnpm && . "$NVM_DIR"/nvm.sh && pnpm'
 fi
